d49d22438d6f7dafb483ed6a6d04a972b5f86481
Sharuka Perera <sharukaperera98@gmail.com>
2020-08-06 21:17:41 +0530

    added dummy data

--- backend/app.js
+++ backend/app.js
@@ -29,45 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use('/students', studentRouter);
-function auth (req, res, next) {
 
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-        return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed: true});
-        next(); // authorized
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-    }
-  }
-  else {
-      if (req.signedCookies.user === 'admin') {
-          next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
-  }
-}
-
-
-
-app.use(auth);
 app.use('/students', adminRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
