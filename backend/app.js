23fe343040766e9059e68e5877a571e1d76828d8
Sharukap <64644681+Sharukap@users.noreply.github.com>
2020-08-08 21:20:15 +0530

    Update app.js
    pull request changes

--- backend/app.js
+++ backend/app.js
@@ -3,46 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
 var studentRouter = require('./routes/studentRouter');
 var adminRouter = require('./routes/adminRouter');
-const mongoose=require('mongoose');
-const Students=require('./models/students');
-const url='mongodb://localhost:27017/conFusion';
-const connect=mongoose.connect(url);
-connect.then((db)=>{
+const mongoose = require('mongoose');
+const Students = require('./models/students');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+connect.then((db) => {
 	console.log('connected to server');
-},(err)=>{console.log(err);});
-
+},(err) => {console.log(err);});
 var app = express();
-
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use('/students', studentRouter);
-
 app.use('/students', adminRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
