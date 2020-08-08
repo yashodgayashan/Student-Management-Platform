bd22e3dd442be4b1b08c09b43c679e29a4396284
Asel Gunaratne <aselgunaratne@gmail.com>
2020-08-08 10:27:30 +0530

    adminRouter with get,post and put

---
+++ backend/adminRouter.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Students = require('../models/students');
+
+const adminRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+adminRouter.route('/')
+/*.get((req,res,next) => {
+    Students.find({})
+    .then((students) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(students);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})*/
+.post((req, res, next) => {
+    Students.create(req.body)
+    .then((student) => {
+        console.log('Student record created', student);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(student);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+.put((req,res,next) => {
+    res.statusCode = 403;
+    res.end("PUT Operation not supported on /students");
+
+});
+
+.delete((req, res, next) => {
+    Students.remove(req.body)
+    
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+
+
+adminRouter.route('/:studentId')
+.get((req,res,next) => {
+    Students.findById(req.params.studentId)
+    .then((student) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(student);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Cannot create a student record with'+ req.params.studentId+'Do not include an ID when creating a new record');
+});
+
+.put((req, res, next) => {
+    Students.findByIdAndUpdate(req.params.studentId,{
+        $set: req.body
+    },{new: true })
+    .then((student)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(student);
+    }, (err)=>next(err))
+    .catch((err)=> next(err));
+});
+
+.delete ((req, res, next) => {
+    Students.findByIdAndRemove(req.params.studentId, function (err, resp) { 
+	if (err) throw err;
+        res.json();
+    });
+});
+
+module.exports = adminRouter;
