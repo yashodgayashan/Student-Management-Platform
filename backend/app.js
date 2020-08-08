14c4345f0eea9112a85a822b946f322afcd98169
Sharuka Perera <sharukaperera98@gmail.com>
2020-08-08 21:37:11 +0530

    login authentication

--- backend/app.js
+++ backend/app.js
@@ -3,48 +3,60 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
-var studentRouter = require('./routes/studentRouter');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var adminRouter = require('./routes/adminRouter');
-const mongoose=require('mongoose');
-const Students=require('./models/students');
-const url='mongodb://localhost:27017/conFusion';
+var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+const Students = require('./models/students');
+const url = 'mongodb://localhost:27017/conFusion';
 const connect=mongoose.connect(url);
-connect.then((db)=>{
+connect.then((db) => {
 	console.log('connected to server');
-},(err)=>{console.log(err);});
-
+},(err) => {console.log(err);});
 var app = express();
-
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use('/students', studentRouter);
-
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+app.use('/users', usersRouter);
+function auth (req, res, next) {
+    console.log(req.session);
+  if(!req.session.students) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.students === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('you are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
+  }
+}
+app.use(auth);
 app.use('/students', adminRouter);
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
