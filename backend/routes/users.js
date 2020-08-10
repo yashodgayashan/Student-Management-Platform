92d66aaa2b168aece2fd6229aaebd228420c06f0
Sharukap <64644681+Sharukap@users.noreply.github.com>
2020-08-10 09:15:19 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,14 +6,14 @@ var User = require('../models/students');
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
 router.post('/login', (req, res, next) => {
 
   if(!req.session.students) {
-    var authHeader = req.headers.authorization;
+	var authHeader = req.headers.authorization;
     
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
@@ -33,28 +33,31 @@ router.post('/login', (req, res, next) => {
         err.status = 403;
         return next(err);
       }
+	    
       else if (students.password !== password) {
         var err = new Error('Your password is incorrect!');
         err.status = 403;
         return next(err);
       }
+	    
       else if (students.username === username && students.password === password) {
         req.session.students = 'authenticated';
         res.statusCode = 200;
 		res.setHeader('Content-Type', 'application/json');
 		const admin = true;
-		if(students.accounttype === admin)
-		{
+		if(students.accounttype === admin) {
 			res.json({approved:true,type:"admin"});
 		}
-		else
-		{
+	      
+		else {
 			res.json({approved:true,type:"student"});
 		}
       }
     })
     .catch((err) => next(err));
+  
   }
+
   else {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -62,14 +65,13 @@ router.post('/login', (req, res, next) => {
   }
 })
 
-
-
 router.get('/logout', (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
     res.redirect('/');
   }
+	
   else {
     var err = new Error('You are not logged in!');
     err.status = 403;
